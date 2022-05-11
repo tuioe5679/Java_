1910fdeebba2dfaf69272087567c30fcf813e696
tuioe <qodlgks123@naver.com>
2022-05-12 01:14:41 +0900

    D 문제 수정

--- Java/src/ch5/D.java
+++ Java/src/ch5/D.java
@@ -11,10 +11,10 @@ public class D {
 	public static void main(String[] args) throws FileNotFoundException {
 		Map<String,Integer> map = new TreeMap<String,Integer>();			//TreeMap 사용
 		Scanner stdin = new Scanner(new File("test.txt"));					//Sanner 클래스로 파일 내용을 가져옴 
-		int count = 1;														//개수 (Value)
+		int count;														//개수 (Value)
 		while(stdin.hasNext()) {											//test.txt 파일의 토큰이 없을때까지 반복  
 			String sa = stdin.next();				
-			count = map.containsKey(sa) ? count+1 : count;					//삼항 연산자 적용 									
+			count = map.containsKey(sa) ? map.get(sa) + 1 : 1;
 			map.put(sa, count);
 		}
 		for(String key: map.keySet()) {										//String 객체 key에 map 키를 넣으면서 반복하는 향상된 for문 
